d640c42f79acbf9288f1170d1a9cd281687fdf60
Mohammed Ali <mali@teladoc.com>
2020-06-05 11:33:26 -0500

    completed function expressions exercises

--- src/deep-javascript-foundations-v3/function-expressions.js
+++ src/deep-javascript-foundations-v3/function-expressions.js
@@ -1,33 +1,42 @@
 function printRecords(recordIds) {
-  students = enrolledStudents(recordIds);
+  let students = findStudentRecords(recordIds);
+  print(students)
+}
+
+function paidStudentsToEnroll() {
+  return paidButNotEnrolled().concat(currentEnrollment);
+}
+
+function remindUnpaid(recordIds) {
+  let students = findStudentRecords(unpaidStudents(recordIds))
+  print(students)
+}
+
+function print(students){
   sortRecordsByName(students).forEach(function print(student){
     console.log(`${student.name} (${student.id}):  ${paid(student)}`)
   });
 }
 
-function paidStudentsToEnroll() {
-  students = paidButNotEnrolled().concat(currentEnrollment);
-  console.log(students)
-  printRecords(students)
-}
-
-function remindUnpaid(recordIds) {
-  // TODO
+function unpaidStudents(recordIds){
+ return studentRecords.map(function findStudents(student){
+    if(recordIds.includes(student.id) && !student.paid) return student.id
+  }).filter(item => !!item);
 }
 
 function paidButNotEnrolled(){
   return studentRecords.map(function findStudents(student){
     if(!currentEnrollment.includes(student.id) && student.paid) return student.id
-  }).filter(item => !!item)
+  }).filter(item => !!item);
 }
 
 function paid(student){
   return student.paid ? "Paid" : "Not Paid"
 }
 
-function enrolledStudents(students){
+function findStudentRecords(students){
   return students.map(function findStudents(id){
-    return getRecord(id, studentRecords);
+    return matchStudentById(id, studentRecords);
   }).filter(item => !!item)
 }
 
@@ -48,7 +57,7 @@ function sortRecordsByName(records){
   });
 }
 
-function getRecord(id, students) {
+function matchStudentById(id, students) {
   function match(student) {
     return id == student.id
   }
